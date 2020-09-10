0be9c69779f3938c1d5f3d0e5c5be8fc98142ec1
Isaac Tait <isaac@mountaintopcoding.com>
2020-09-09 21:44:14 -0700

    cleaning up the blog posts a little

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -35,29 +35,29 @@ const {
         } = data.site.siteMetadata
 
     return (
-        <div class="bg-red-700 lg:flex lg:items-center lg:justify-between">
-            <span class="lg:text-xl lg:font-bold lg:text-gray-600">
+        <div class="bg-red-500 lg:flex lg:items-center lg:justify-between">
+            <span class="lg:text-xl lg:font-bold lg:text-white">
                 <Link to="/learning-japanese">{linkOne}</Link>
             </span>
-            <span class="lg:text-xl lg:font-bold lg:text-gray-600">
+            <span class="lg:text-xl lg:font-bold lg:text-white">
                 <Link to="/tenkara-fishing-store">{linkTwo}</Link>
             </span>
-            <span class="lg:text-xl lg:font-bold lg:text-gray-600">
+            <span class="lg:text-xl lg:font-bold lg:text-white">
                 <Link to="/keiryu-fishing-season">{linkThree}</Link>
             </span>
-            <span class="lg:text-xl lg:font-bold lg:text-gray-600">
+            <span class="lg:text-xl lg:font-bold lg:text-white">
                 <Link to="/japanese-fishing-license">{linkFour}</Link>
             </span>
-            <span class="lg:text-xl lg:font-bold lg:text-gray-600">
+            <span class="lg:text-xl lg:font-bold lg:text-white">
                 <Link to="/toll-roads">{linkFive}</Link>
             </span>
-            <span class="lg:text-xl lg:font-bold lg:text-gray-600">
+            <span class="lg:text-xl lg:font-bold lg:text-white">
                 <Link to="/links">{linkSix}</Link>
             </span>
-            <span class="lg:text-xl lg:font-bold lg:text-gray-600">
+            <span class="lg:text-xl lg:font-bold lg:text-white">
                 <Link to="/about">{linkSeven}</Link>
             </span>
-            <span class="lg:text-xl lg:font-bold lg:text-gray-600">
+            <span class="lg:text-xl lg:font-bold lg:text-white">
                 <Link to="/tenkara-101">{linkEight}</Link>
             </span>
         </div>  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          '500': '#D2132D',
+        }
+      }
+    },
   },
   variants: {},
   plugins: [require("tailwindcss")],
