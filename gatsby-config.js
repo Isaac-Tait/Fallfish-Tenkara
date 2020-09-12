b7bf8b673a1afa97b7a9d7c50e124d1ed6fcb7dc
Isaac Tait <isaac@mountaintopcoding.com>
2020-09-11 17:12:51 -0700

    reconfig to show /blog/post-name in url

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/content`,
         name: `blog`,
       },
     },
