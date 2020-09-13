6048f1cf16661d50d28b58fb39d21b4114197da2
Isaac Tait <isaac@mountaintopcoding.com>
2020-09-13 15:08:28 -0700

    getting ready to work on conditional render menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,28 +46,34 @@ const Layout = ({ location, children }) => {
       <p class="text-3xl font-extrabold">Blog Posts</p>
       <main>{children}</main>
       <div class="bg-red-500">
-        <footer>
-          © 2014 - {new Date().getFullYear()}, Built with
-          {` `}
-          <a 
-            href="https://www.gatsbyjs.org"
-            class="hover:text-white"
-            target="_blank"  
-            rel="noopener noreferrer" 
-          > Gatsby</a> and 
-          <a 
-            href="https://tailwindcss.com"
-            class="hover:text-white"
-            target="_blank"  
-            rel="noopener noreferrer" 
-          > TailwindCSS</a> 
-          <span class="ml-20"> --- Another 
-          <a 
-            href="https://www.mountaintopcoding.com"
-            class="hover:text-white"
-            target="_blank"  
-            rel="noopener noreferrer" 
-          > mountainTopCoding(<span role="img" aria-label="mountain with snow-cap">&#127956;</span>);</a> project</span>
+        <footer class="flex items-center justify-between">
+          <div class="ml-2">
+            © 2014 - {new Date().getFullYear()}, Built with
+            {` `}
+            <a 
+              href="https://www.gatsbyjs.org"
+              class="hover:text-white"
+              target="_blank"  
+              rel="noopener noreferrer" 
+            > Gatsby</a> and 
+            <a 
+              href="https://tailwindcss.com"
+              class="hover:text-white"
+              target="_blank"  
+              rel="noopener noreferrer" 
+            > TailwindCSS</a>
+          </div>
+          <div>
+            <span class="ml-20 mr-2"> Another 
+              <a 
+                href="https://www.mountaintopcoding.com"
+                class="hover:text-white"
+                target="_blank"  
+                rel="noopener noreferrer" 
+              > mountainTopCoding(<span role="img" aria-label="mountain with snow-cap">&#127956;</span>);</a> project
+            </span>
+          </div>
+          
         </footer>
       </div>
     </div>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -39,28 +39,6 @@ const {
 
     return (
         <div class="bg-red-500 mb-2 lg:flex lg:items-center lg:justify-between">
-            <button class="lg:hidden block">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path 
-                        fill-rule="evenodd" 
-                        clip-rule="evenodd" 
-                        d="M3 5C3 4.44772 3.44772 4 4 4H16C16.5523 4 17 4.44772 17 5C17 5.55228 16.5523 6 16 6H4C3.44772 6 3 5.55228 3 5Z" 
-                        fill="#ffffff"
-                    />
-                    <path 
-                        fill-rule="evenodd" 
-                        clip-rule="evenodd" 
-                        d="M3 10C3 9.44772 3.44772 9 4 9H16C16.5523 9 17 9.44772 17 10C17 10.5523 16.5523 11 16 11H4C3.44772 11 3 10.5523 3 10Z" 
-                        fill="#ffffff"
-                    />
-                    <path 
-                        fill-rule="evenodd" 
-                        clip-rule="evenodd" 
-                        d="M3 15C3 14.4477 3.44772 14 4 14H16C16.5523 14 17 14.4477 17 15C17 15.5523 16.5523 16 16 16H4C3.44772 16 3 15.5523 3 15Z" 
-                        fill="#ffffff"
-                    />
-                </svg>
-            </button>
                 <span class="sm:mt-1 sm:px-2 sm:block sm:text-white sm:hover:text-black lg:m-2 lg:text-lg lg:font-bold lg:text-white">
                     <Link to="/learning-japanese">{linkOne}</Link>
                 </span>
@@ -88,6 +66,30 @@ const {
                 <span class="sm:mt-1 sm:px-2 sm:block sm:text-white sm:hover:text-black lg:mr-2 lg:text-lg lg:font-bold lg:text-white">
                     <Link to="/search">{linkNine}</Link>
                 </span>
+
+                <button  type="button" class="">
+                <svg class="h-6 w-6 fill-current" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path 
+                        fill-rule="evenodd" 
+                        clip-rule="evenodd" 
+                        d="M3 5C3 4.44772 3.44772 4 4 4H16C16.5523 4 17 4.44772 17 5C17 5.55228 16.5523 6 16 6H4C3.44772 6 3 5.55228 3 5Z" 
+                        fill="#ffffff"
+                    />
+                    <path 
+                        fill-rule="evenodd" 
+                        clip-rule="evenodd" 
+                        d="M3 10C3 9.44772 3.44772 9 4 9H16C16.5523 9 17 9.44772 17 10C17 10.5523 16.5523 11 16 11H4C3.44772 11 3 10.5523 3 10Z" 
+                        fill="#ffffff"
+                    />
+                    <path 
+                        fill-rule="evenodd" 
+                        clip-rule="evenodd" 
+                        d="M3 15C3 14.4477 3.44772 14 4 14H16C16.5523 14 17 14.4477 17 15C17 15.5523 16.5523 16 16 16H4C3.44772 16 3 15.5523 3 15Z" 
+                        fill="#ffffff"
+                    />
+                </svg>
+            </button>
+
         </div>  
     );
 };
