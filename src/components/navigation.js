d10454611f0cee4e1524ffd3b71e7439008b4d88
Isaac Tait <isaac@mountaintopcoding.com>
2020-09-17 17:04:21 -0700

    finally have a responsive menu albeit an ugly one

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -13,7 +13,7 @@ import {
     GiFishing 
 } from "react-icons/gi"
 
-export const Navigation = () => {
+const Navigation = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const data = useStaticQuery(graphql`
@@ -56,86 +56,7 @@ export const Navigation = () => {
     return (
       <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div class="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
-          <ul class="flex items-center hidden space-x-8 lg:flex">
-            <li>
-              <a
-                href="/"
-                aria-label="Our product"
-                title="Our product"
-                class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Product
-              </a>
-            </li>
-            <li>
-              <a
-                href="/"
-                aria-label="Our product"
-                title="Our product"
-                class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Features
-              </a>
-            </li>
-            <li>
-              <a
-                href="/"
-                aria-label="Product pricing"
-                title="Product pricing"
-                class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Pricing
-              </a>
-            </li>
-          </ul>
-          <a
-            href="/"
-            aria-label="Company"
-            title="Company"
-            class="inline-flex items-center lg:mx-auto"
-          >
-            <svg
-              class="w-8 text-deep-purple-accent-400"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              stroke="currentColor"
-              fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12" />
-            </svg>
-            <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 md:text-white">
-              Fallfish Tenkara
-            </span>
-          </a>
-          <ul class="flex items-center hidden ml-auto space-x-8 lg:flex">
-            <li>
-              <a
-                href="/"
-                aria-label="Sign in"
-                title="Sign in"
-                class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Sign in
-              </a>
-            </li>
-            <li>
-              <a
-                href="/"
-                class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                aria-label="Sign up"
-                title="Sign up"
-              >
-                Sign up
-              </a>
-            </li>
-          </ul>
-          <div class="ml-auto lg:hidden">
+          <div class="ml-auto">
             <button
               aria-label="Open Menu"
               title="Open Menu"
@@ -165,7 +86,7 @@ export const Navigation = () => {
             </button>
             {isMenuOpen && (
               <div class="absolute top-0 left-0 w-full">
-                <div class="p-5 bg-white border rounded shadow-sm">
+                <div class="p-5 bg-red-500 rounded shadow-sm">
                   <div class="flex items-center justify-between mb-4">
                     <div>
                       <a
@@ -174,23 +95,8 @@ export const Navigation = () => {
                         title="Company"
                         class="inline-flex items-center"
                       >
-                        <svg
-                          class="w-8 text-deep-purple-accent-400"
-                          viewBox="0 0 24 24"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeMiterlimit="10"
-                          stroke="currentColor"
-                          fill="none"
-                        >
-                          <rect x="3" y="1" width="7" height="12" />
-                          <rect x="3" y="17" width="7" height="6" />
-                          <rect x="14" y="1" width="7" height="6" />
-                          <rect x="14" y="11" width="7" height="12" />
-                        </svg>
                         <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                          Company
+                          Information
                         </span>
                       </a>
                     </div>
@@ -212,13 +118,13 @@ export const Navigation = () => {
                   </div>
                   <nav>
                     <ul class="space-y-4">
-                        <li icon={<GiSharpShuriken />} class="ml-2">
+                        <li>
                             <span class="mt-2">
                                 <Link to="/learning-japanese">{linkOne}</Link>
                             </span> 
                         </li>        
 
-                        <li icon={<GiBamboo />} class="ml-2">
+                        <li>
                             <span class="mt-2">
                                 <Link to="/tenkara-fishing-store">{linkTwo}</Link>
                             </span>
@@ -275,3 +181,5 @@ export const Navigation = () => {
       </div>
     );
   };
+
+  export default Navigation
