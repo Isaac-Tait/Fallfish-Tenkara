7608f832fe43d88c30794aaa8f75546eb7e194e3
Isaac Tait <isaac@mountaintopcoding.com>
2020-09-27 20:16:24 -0700

    small changes to front page

--- src/components/frontPage.js
+++ src/components/frontPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const FrontPage = () => {
     return (
@@ -7,25 +8,25 @@ const FrontPage = () => {
           <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
             <a href="/" aria-label="Article">
               <img
-                src="https://images.pexels.com/photos/932638/pexels-photo-932638.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                src="https://fallfish-tenkara-images.s3-us-west-1.amazonaws.com/FfT+-+Shems+Jud+/tanzawa+mountains-japan-tenkara-fishing-business+trip-exploring-guest+blog+post-waterfall.JPG"
                 className="object-cover w-full h-64 rounded"
-                alt=""
+                alt="Waterfall"
               />
             </a>
             <div className="py-5">
               <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
-                13 Jul 2020
+                04 April 2019
               </p>
               <a
-                href="/"
+                href="/blog/kamoshika-backyard"
                 aria-label="Article"
                 className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
               >
-                <p className="text-2xl font-bold leading-5">Diving to the deep</p>
+                <p className="text-2xl font-bold leading-5">Kamoshika's Backyard</p>
               </a>
               <p className="mb-4 text-gray-700">
-                Sed ut perspiciatis unde omnis iste natus error sit sed quia
-                consequuntur magni voluptatem doloremque.
+              I have been enchanted with Japanese trout and char since a stint teaching English 
+              in Japan some twenty years ago.
               </p>
             </div>
           </div>
@@ -83,7 +84,14 @@ const FrontPage = () => {
           </div>
         </div>
         <div>
-            <p class="font-bold text-red-500 mt-6">&#10145; More articles</p>
+            <Link 
+              to={`/blog`}
+              class="tracking-wide m-2 inline-block px-3 py-1 rounded-lg shadow-lg bg-red-500 text-white hover:bg-gray-300 hover:text-black"
+            ><span role="img" aria-label="right arrow">&#10145;</span> More articles</Link>
+            <Link 
+              to={`/search`}
+              class="tracking-wide m-2 inline-block px-3 py-1 rounded-lg shadow-lg bg-red-500 text-white hover:bg-gray-300 hover:text-black"
+            ><span role="img" aria-label="right leaning magnifying glass">&#128270;</span> Search</Link>
         </div>
       </div>
     );
