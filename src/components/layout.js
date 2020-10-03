1163c733f1acdc4bb5398e3373d3c8632d7f5f99
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-03 09:36:09 -0700

    converted footer into a component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import Navigation from "../components/navigation"
 import Slider from "../components/slider"
 import FrontPage from "../components/frontPage"
 import Image from "gatsby-image"
+import Footer from "../components/footer.js"
 
 const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -72,38 +73,7 @@ const logos = [
         <FrontPage />
         <main>{children}</main>
       </div>
-      <div class="bg-red-500">
-        <footer class="flex items-center justify-between text-xs md:text-base">
-          <div class="ml-2">
-            © 2014 - {new Date().getFullYear()}, Built with
-            {` `}
-            <a 
-              href="https://www.gatsbyjs.org"
-              class="hover:text-white"
-              target="_blank"  
-              rel="noopener noreferrer" 
-            > Gatsby</a> and 
-            <a 
-              href="https://tailwindcss.com"
-              class="hover:text-white"
-              target="_blank"  
-              rel="noopener noreferrer" 
-            > TailwindCSS</a>
-          </div>
-
-          <div>
-            <span class="ml-20 mr-2"> Another 
-              <a 
-                href="https://www.mountaintopcoding.com"
-                class="hover:text-white"
-                target="_blank"  
-                rel="noopener noreferrer" 
-              > mountainTopCoding(<span role="img" aria-label="mountain with snow-cap">&#127956;</span>);</a> project
-            </span>
-          </div>
-          
-        </footer>
-      </div>
+        <Footer />
     </div>
   )
 }
