9bac9a6bb2366195fdc875a0cb364067fbe0dce0
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-04 15:46:57 -0700

    adding eslint functionality to clean up code

--- src/components/blogPosts.js
+++ src/components/blogPosts.js
@@ -1,39 +1,39 @@
 import React from "react"
-import { useStaticQuery, Link } from "gatsby"
+//import { useStaticQuery, Link } from "gatsby"
 
 import SEO from "../components/seo"
 
 const BlogPosts = ({ children }) => {
-    const data = useStaticQuery(graphql`
-        query Pager {
-            allMarkdownRemark {
-                totalCount 
-                nodes {
-                    frontmatter {
-                        title
-                    }
-                    fields {
-                        slug
-                    }
-                }
-            }
-        }
-    `)
-    const pages = data.Pager.allMarkdownRemark.nodes
+    // const data = useStaticQuery(graphql`
+    //     query Pager {
+    //         allMarkdownRemark {
+    //             totalCount 
+    //             nodes {
+    //                 frontmatter {
+    //                     title
+    //                 }
+    //                 fields {
+    //                     slug
+    //                 }
+    //             }
+    //         }
+    //     }
+    // `)
+    // const pages = data.Pager.allMarkdownRemark.nodes
 
     return (
         <div>
             <SEO />
-            <div>
-                {pages.map((blog) => (
+            
+            
                     <div>
-                        <Link to={`/${blog.slug.current}`}>
+                        
                             <p>Blog Posts </p>
-                        </Link>
+                        
                     </div>
-                ))}
-            </div>
-            <p>{children}</p>
+            
+            
+            <p>A list of blog posts: {children}</p>
         </div>
     )
 } 
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,7 +7,7 @@ const Blog = () => {
     return (
         <div class="bg-gray-200 mb-4 w-full lg:w-2/3 mx-auto overflow-hidden rounded-lg shadow-xl">
             <p>This page works....</p>
-            <p>{process.env.GATSBY_PAGE_SIZE}</p>
+            <BlogPosts />
             <Footer /> 
         </div>
     )
