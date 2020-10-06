469016b45f56ce959c6cc4d6f3accaf846201308
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-05 17:46:21 -0700

    still working on pagination

---
+++ src/templates/blog-list.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const BlogList = () => {
+    render() {
+        const { currentPage, numPages } = this.props.pageContext
+        const isFirst = currentPage === 1
+        const isLast = currentPage === numPages
+        const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
+        const nextPage = (currentPage + 1).toString()
+    }
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                    {!isFirst && (
+                        <Link to={prevPage} rel="prev">
+                            ← Previous Page
+                        </Link>
+                    )}
+                    {!isLast && (
+                        <Link to={nextPage} rel="next">
+                            Next Page →
+                        </Link>
+                    )}
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default BlogList
+
+export const pageQuery = graphql`
+query blogPageQuery($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+    sort: { fields: [frontmatter___date], order: DESC }
+    limit: $limit
+    skip: $skip
+    ) {
+    edges {
+        node {
+        excerpt
+        frontmatter {
+            date(formatString: "DD MMMM, YYYY")
+            title
+             }
+          }
+       }
+    }
+  }
+`
