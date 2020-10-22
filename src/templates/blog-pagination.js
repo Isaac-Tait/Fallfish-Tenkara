1393022118f926e28e7be122b9015ffd8e95c0ce
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-21 20:39:25 -0700

    moved query location

--- src/templates/blog-pagination.js
+++ src/templates/blog-pagination.js
@@ -4,7 +4,39 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Pager from "../components/pager"
 
-export const pageQuery = graphql`
+class BlogPagination extends React.Component {
+  render() {
+    const { data, location } = this.props
+    const posts = data.allMarkdownRemark.edges;
+  
+    return (
+      <Layout location={location}>
+        {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <article key={node.fields.slug}>
+                <header>
+                  <h3>
+                    <Link to={node.fields.slug}> {title} </Link>
+                  </h3>
+                  <small>{node.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p dangerouslySetInnerHTML={{ __html: node.frontmatter.description || node.excerpt }} />
+                </section>
+              </article>
+            )
+        })}
+  
+        <Pager pageContext={pageContext} />
+      </Layout>
+    )
+  }
+};
+
+export default BlogPagination;
+
+export const query = graphql`
   query ($skip: Int!, $limit: Int!) {
     site {
       siteMetadata {
@@ -31,32 +63,3 @@ export const pageQuery = graphql`
     }
   }
 `
-
-const BlogPagination = ({ data, pageContext, location }) => {
-  const posts = data.allMarkdownRemark.edges;
-
-  return (
-    <Layout location={location}>
-      {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <article key={node.fields.slug}>
-              <header>
-                <h3>
-                  <Link to={node.fields.slug}> {title} </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-              </header>
-              <section>
-                <p dangerouslySetInnerHTML={{ __html: node.frontmatter.description || node.excerpt }} />
-              </section>
-            </article>
-          )
-      })}
-
-      <Pager pageContext={pageContext} />
-    </Layout>
-  )
-};
-
-export default BlogPagination;
