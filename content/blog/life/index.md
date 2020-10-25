9bd38e7c0e79998d1e7a1b552f39842bb7eda5a7
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-25 06:58:23 -0700

    getting closer to being happy with the home page

---
+++ content/blog/life/index.md
@@ -0,0 +1,53 @@
+---
+title: Life
+description: The old adage Good things must always come to an end rings especially true for me as I prepare to leave Japan...
+date: 2018-04-16T03:13:00+02:00
+tags: [Random]
+---
+<div class=“text-lg m-2”>
+<p class="mb-2">When I was younger I had no concept of time, I thought I would be young forever. My first real realization of time came when I was 17 and I joined the US Marine Corps. Suddenly, as I was standing on those <a href="https://terminallance.com/2015/08/11/terminal-lance-394-cant-stop-time-ii/" target="_blank" rel="noopener">yellow footprints</a>, I realized:
+
+<em>"13 weeks is a long time!" </em>
+
+As my drill instructor was screaming in my face with his knife hand quivering a hairs breadth (this is the PC term btw) from my nose I thanked my God that they could do anything they wanted to me <span style="text-decoration: underline;">but</span> stop the clock.
+
+That was 16+ years ago now. The clock never stops ticking and all good things (and bad things too) must all come to an end.
+
+On November 13, 2014 I caught my first glimpse of Japan from the plane's window. As I looked down on the Ibaraki coast line I thought to myself:
+
+<em>"Three years is a long time!"</em>
+
+Well, three years was more like 3 1/2 years - but, the clock kept ticking and now I too have closed the Japan chapter in my life. Of course I will go to Japan again, but I will be a tourist then and not a resident. Not to say that there is anything wrong with that, it will just be different...
+
+After I had lived in Japan for year or so the novelty was replaced with familiarity. Since then I have always been a little envious of people who have just arrived in Japan for the first time. For them everything in Japan is foreign, wonderful, amazing, unbelievable, otherworldly.
+
+As Jonathan Antunez said:
+
+<em>"[Japan is] the closest you can get to another world without leaving earth. I loved every minute."</em>
+
+I love Japan, and I loved every minute I was there - all one million seven hundred and sixty one thousand one hundred and twenty minutes.
+
+As they say:
+
+<em>"You can take the man out of the Corps, but you cannot take the Corps out of the man." </em>
+
+I feel the same rings true for me and Japan:
+
+<em>"You can take the man out of Japan, but you cannot take Japan out of the man."</em>
+
+<hr />
+
+[caption id="attachment_890" align="aligncenter" width="768"]<img class="wp-image-890 size-large" src="http://104.248.67.90/wp-content/uploads/2015/01/Gin-Clear-Water_Tenkara_Mountain-Stream_Uratanzawa-768x1024.jpg" alt="" width="768" height="1024" /> The first photo ever published on Fallfish Tenkara[/caption]
+
+I will stop waxing nostalgic and reminiscing now, because yes I will miss Japan and my friends there, but I am now beginning a new chapter. Since I published the <a href="http://104.248.67.90/fishing-uratanzawa/" target="_blank" rel="noopener">first blog post</a> on Fallfish Tenkara on January 20, 2015 I have aimed at making it a resource for those interested in Japan and Japanese tenkara. However, my desire to keep writing about fishing and tenkara has not abated - so in an effort to keep Fallfish Tenkara all about Japan I have launched a new site called <a href="http://www.cutthroattenkara.com" target="_blank" rel="noopener">Cutthroat Tenkara</a>.
+
+<img class="size-large wp-image-5416 aligncenter" src="http://104.248.67.90/wp-content/uploads/2018/03/CtT-1-1024x346.jpg" alt="" width="1024" height="346" />
+
+Cutthroat Tenkara will be for my continuing tenkara adventures in the US and anywhere else but Japan. I would love to keep adding cool Japan content to Fallfish Tenkara though, so if you have (or recently had) a trip to the Land of the Rising Sun and would like to write a guest post please let me know - if it is real good I may even consider paying for exclusive rights (please see <a href="http://104.248.67.90/guest-blogging/" target="_blank" rel="noopener">this</a> page for more info on submitting guest trip reports from Japan to Fallfish Tenkara)
+
+Cutthroat Tenkara is still a little rough around the edges, but what is not rough is the logo (a huge shout out to my friend <a href="https://www.forman.farm/" target="_blank" rel="noopener">Charles Forman</a> who designed both the Fallfish Tenkara logo and both of the Cutthroat Tenkara logos). I dig the new Cutthroat Tenkara logo so much I have decided to do a limited edition sticker release 100% free (for the first 50 people). Shoot me an email using the contact form below with your mailing address and I will get a free sticker in the mail to you asap (please allow a few weeks for delivery). Once this batch is gone, that is it though and it is first come first served...</p>
+
+<img class="w-8/12 rounded-lg shadow-lg mx-auto" src="" alt="" />
+</div>
+
+target="_blank" rel="noopener noreferrer"
--- src/components/frontPage.js
+++ src/components/frontPage.js
@@ -3,96 +3,97 @@ import { Link } from "gatsby"
 
 const FrontPage = () => {
     return (
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
-          <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
-            <a href="/" aria-label="Article">
+      <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div class="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+          <div class="overflow-hidden transition-shadow duration-300 bg-white rounded">
+            <a href="/kamoshika-backyard" aria-label="Article">
               <img
-                src="https://fallfish-tenkara-images.s3-us-west-1.amazonaws.com/FfT+-+Shems+Jud+/tanzawa+mountains-japan-tenkara-fishing-business+trip-exploring-guest+blog+post-waterfall.JPG"
-                className="object-cover w-full h-64 rounded"
+                src="https://fallfish-tenkara-images.s3-us-west-1.amazonaws.com/FfT+-+Shems+Jud+/tanzawa+mountains-japan-tenkara-fishing-business+trip-exploring-guest+blog+post-kamoshika.JPG"
+                class="object-cover w-full h-64 rounded"
                 alt="Waterfall"
               />
             </a>
-            <div className="py-5">
-              <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+            <div class="py-5">
+              <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
                 04 April 2019
               </p>
               <a
                 href="/kamoshika-backyard"
                 aria-label="Article"
-                className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+                class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
               >
-                <p className="text-2xl font-bold leading-5">Kamoshika's Backyard</p>
+                <p class="text-2xl font-bold leading-5">Kamoshika's Backyard</p>
               </a>
-              <p className="mb-4 text-gray-700">
+              <p class="mb-4 text-gray-700">
               I have been enchanted with Japanese trout and char since a stint teaching English 
-              in Japan some twenty years ago.
+              in Japan some twenty years ago...
               </p>
             </div>
           </div>
-          <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
-            <a href="/" aria-label="Article">
+          
+          
+          <div class="overflow-hidden transition-shadow duration-300 bg-white rounded">
+            <a href="/mountain-huts" aria-label="Article">
               <img
-                src="https://images.pexels.com/photos/1576937/pexels-photo-1576937.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-                className="object-cover w-full h-64 rounded"
+                src="https://fallfish-tenkara-images.s3-us-west-1.amazonaws.com/FfT+-+Mountain+Huts/Japan-Mountain-Huts-Tenkara-Exploring-Katanokoya-Hut.jpg"
+                class="object-cover w-full h-64 rounded"
                 alt=""
               />
             </a>
-            <div className="py-5">
-              <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
-                4 Nov 2020
+            <div class="py-5">
+              <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+                21 May 2018
               </p>
               <a
-                href="/"
+                href="/mountain-huts"
                 aria-label="Article"
-                className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+                class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
               >
-                <p className="text-2xl font-bold leading-5">Conquer the World</p>
+                <p class="text-2xl font-bold leading-5">Mountain Huts</p>
               </a>
-              <p className="mb-4 text-gray-700">
-                Sed ut perspiciatis unde omnis iste natus error sit sed quia
-                consequuntur magni voluptatem doloremque.
-              </p>
+              <p class="mb-4 text-gray-700">The mountain hut system in Japan is not only extensive but also many can be quite luxurious...</p>
             </div>
           </div>
-          <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
-            <a href="/" aria-label="Article">
+          
+          
+          <div class="overflow-hidden transition-shadow duration-300 bg-white rounded">
+            <a href="/life" aria-label="Article">
               <img
-                src="https://images.pexels.com/photos/2123755/pexels-photo-2123755.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                className="object-cover w-full h-64 rounded"
+                src="https://fallfish-tenkara-images.s3-us-west-1.amazonaws.com/FfT+-+Life/Kadosan-friend-adventurer-sensei-badass.JPG"
+                class="object-cover w-full h-64 rounded"
                 alt=""
               />
             </a>
-            <div className="py-5">
-              <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
-                28 Dec 2020
+            <div class="py-5">
+              <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+                16 April 2018
               </p>
               <a
-                href="/"
+                href="/life"
                 aria-label="Article"
-                className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+                class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
               >
-                <p className="text-2xl font-bold leading-5">
-                  Explore the beautiful
-                </p>
+                <p class="text-2xl font-bold leading-5">Life</p>
               </a>
-              <p className="mb-4 text-gray-700">
-                Sed ut perspiciatis unde omnis iste natus error sit sed quia
-                consequuntur magni voluptatem doloremque.
+              <p class="mb-4 text-gray-700">
+                The old adage: "Good things must always come to an end" rings especially true for me as I prepare to leave Japan...
               </p>
             </div>
           </div>
         </div>
-        <div>
+
+
+        <div class="flex flex-wrap w-full justify-between">
             <Link 
               to={`/blog`}
               class="tracking-wide m-2 inline-block px-3 py-1 rounded-lg shadow-lg bg-red-500 text-white hover:bg-gray-300 hover:text-black"
-            ><span role="img" aria-label="right arrow">&#10145;</span> More articles</Link>
+            ><span role="img" aria-label="map of japan">&#128510;</span> More articles</Link>
             <Link 
               to={`/search`}
               class="tracking-wide m-2 inline-block px-3 py-1 rounded-lg shadow-lg bg-red-500 text-white hover:bg-gray-300 hover:text-black"
             ><span role="img" aria-label="right leaning magnifying glass">&#128270;</span> Search</Link>
         </div>
+
       </div>
     );
   };
