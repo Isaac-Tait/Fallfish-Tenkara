d9920ce673427aa8d194c257c1a9a03719c1a426
Isaac Tait <isaac@mountaintopcoding.com>
2021-03-07 17:08:32 -0800

    spruce up search

--- src/components/search-preview.js
+++ src/components/search-preview.js
@@ -7,11 +7,11 @@ import { Highlight } from "react-instantsearch-dom";
 const SearchPreview = ({ hit }) => {
     return (
         <div>
-            <Link to={hit.slug}>
+            <Link to={hit.fields.slug}>
                 <Highlight hit={hit} attribute="title" tagName="mark" />
             </Link>
-            
-            <p dangerouslySetInnerHTML={{__html: hit.frontmatter.description || hit.excerpt,}}/>
+
+            <p dangerouslySetInnerHTML={{__html: hit.frontmatter.title && hit.frontmatter.description || hit.excerpt,}}/>
 
             <div class="font-semibold text-red-500">
                 <Highlight hit={hit} attribute="excerpt" tagName="mark" />
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,21 +1,69 @@
 import React from "react";
+import { graphql } from "gatsby"
 
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { 
+    InstantSearch, 
+    SearchBox, 
+    Hits 
+} from 'react-instantsearch-dom';
 
+import SEO from "../components/seo";
 import SearchPreview from "../components/search-preview";
 
-const searchClient = algoliasearch('BJ28ZE2PI2', 'e4f69a18daf130206dad5fc040b7d4b5');
-
-const Search = () => (
-    <div class="w-8/12 mx-auto">
-        <InstantSearch searchClient={searchClient} indexName="Fallfish_Tenkara_Blog">
-            <div class="mt-12">
-                <SearchBox />
-            </div>
-            <Hits hitComponent={SearchPreview}/>
-        </InstantSearch>
-    </div>
+const searchClient = algoliasearch(
+    'BJ28ZE2PI2', 
+    'e4f69a18daf130206dad5fc040b7d4b5'
 );
 
+class Search extends React.Component {
+    render() {
+        const { data } = this.props;
+        const siteTitle = data.site.siteMetadata.title;
+
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <SEO 
+                    title="All Posts"
+                    keywords={[`tenkara`,`japan`,`fly fishing`,`sawanobori`,]}
+                />
+                <div class="w-8/12 mx-auto">
+                    <InstantSearch searchClient={searchClient} indexName="Fallfish_Tenkara_Blog">
+                        <div class="mt-12">
+                            <p>Enter your search query below:</p>
+                            <SearchBox />
+                        </div>
+                        <Hits hitComponent={SearchPreview}/>
+                    </InstantSearch>
+                </div>
+            </Layout>
+        )
+    }
+};
+
 export default Search
+
+export const PageQuery = graphql`
+  query {
+        site {
+        siteMetadata {
+        title
+        }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter__date], order: DESC }) {
+        edges {
+        node {
+            excerpt
+            fields {
+            slug
+            }
+            frontmatter {
+            date(formatString: "MMMM DD YYYY")
+            title
+            description
+            }
+          }
+       }
+    }
+  }
+`
