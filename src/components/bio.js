e05ce1bb1775bc0fbe8d05b0bccbea2f85374ff6
Isaac Tait <isaac@mountaintopcoding.com>
2021-03-09 13:08:24 -0800

    clean up bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,22 +1,13 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
-
-import { rhythm } from "../utils/typography"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
+          fixed(width: 75, height: 75) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -37,30 +28,14 @@ const Bio = () => {
 
   const { author, social } = data.site.siteMetadata
   return (
-    <div class="mb-6 mr-4 flex">
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author.name}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <p>
-        Written by <strong>{author.name}</strong> {author.summary}
-        {` `}
-        <a 
-          href={`https://twitter.com/${social.twitter}`}
-          class="text-red-500 hover:bg-red-500 hover:text-white"
-        >
-          You should follow him on Twitter
-        </a>
-      </p>
+    <div class="mb-6 mr-4 flex flex-row">
+      
+      <div class="mr-6">
+        <Image fixed={data.avatar.childImageSharp.fixed} alt={author.name}/>
+      </div>
+      
+      <p>Written by <span class="font-bold">{author.name}</span> {author.summary} You should follow him on&nbsp;
+        <a href={`https://twitter.com/${social.twitter}`} class="text-red-500 hover:bg-red-500 hover:text-white">Twitter</a>.</p>
     </div>
   )
 }
