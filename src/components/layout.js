4876136a984559397cef85284d657b77f29f0e94
Isaac Tait <isaac@mountaintopcoding.com>
2021-05-17 15:54:35 -0700

    run gatsbyCodeMod to rm deprecated plugins/syntax

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Navigation from "../components/navigation"
 import Slider from "../components/slider"
-import Image from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image";
 import Footer from "../components/footer"
 import Pagination from "../templates/blog-pagination"
 
@@ -11,31 +11,34 @@ const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
 
-  const data = useStaticQuery(graphql`
-    query {
-      desktopLogo: file(absolutePath: { regex: "/FfT_Logo_Desktop.png/"}) {
-        childImageSharp {
-          fluid(maxWidth: 500, quality: 50) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-      mobileLogo: file(
-        absolutePath: { regex: "/FfT_Logo_Mobile.png/"}
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 250, quality: 100) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
+  const data = useStaticQuery(graphql`{
+  desktopLogo: file(absolutePath: {regex: "/FfT_Logo_Desktop.png/"}) {
+    childImageSharp {
+      gatsbyImageData(
+        width: 500
+        quality: 50
+        placeholder: TRACED_SVG
+        layout: CONSTRAINED
+      )
     }
+  }
+  mobileLogo: file(absolutePath: {regex: "/FfT_Logo_Mobile.png/"}) {
+    childImageSharp {
+      gatsbyImageData(
+        width: 250
+        quality: 100
+        placeholder: TRACED_SVG
+        layout: CONSTRAINED
+      )
+    }
+  }
+}
 `)
 
 const logos = [
-  data.mobileLogo.childImageSharp.fluid,
+  data.mobileLogo.childImageSharp.gatsbyImageData,
   {
-    ...data.desktopLogo.childImageSharp.fluid,
+    ...data.desktopLogo.childImageSharp.gatsbyImageData,
     media: `(min-width: 768px)`
   }
 ]
@@ -43,20 +46,14 @@ const logos = [
   if (location.pathname === rootPath) {
     header = (
       <div class="ml-20 mr-20 mb-2">
-        <Image
-          fluid={logos}        
-          alt="Fallfish Tenkara"
-        />
+        <GatsbyImage image={logos} alt="Fallfish Tenkara" />
       </div>
     )
   } else {
     header = (
       <div class="ml-20 mr-20 mb-2">
         <Link to={`/`}>
-          <Image
-            fluid={logos}
-            alt="Fallfish Tenkara"
-          />
+          <GatsbyImage image={logos} alt="Fallfish Tenkara" />
         </Link>
       </div>
         
--- src/components/slider.js
+++ src/components/slider.js
@@ -1,31 +1,28 @@
 import React, { useState } from "react"
 
 import { graphql, useStaticQuery } from "gatsby"
-import Image from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image";
 
 function Slider() {
     const [index, setIndex] = useState(0)
     const { allFile } = useStaticQuery(
-        graphql`
-        query {
-            allFile(
-                sort: { fields: name, order: DESC }
-                filter: { relativeDirectory: { eq: "sliders" }}
-                ) {
-                  edges {
-                      node {
-                          id
-                          name
-                          childImageSharp {
-                            fluid(maxWidth: 900, maxHeight: 255, quality: 100) {
-                                ...GatsbyImageSharpFluid
-                            }
-                        }
-                    }
-                }
-            }
-        }  
-    `)
+        graphql`{
+  allFile(
+    sort: {fields: name, order: DESC}
+    filter: {relativeDirectory: {eq: "sliders"}}
+  ) {
+    edges {
+      node {
+        id
+        name
+        childImageSharp {
+          gatsbyImageData(width: 900, height: 255, quality: 100, layout: CONSTRAINED)
+        }
+      }
+    }
+  }
+}
+`)
 
     const length = allFile.edges.length - 1
     const handleNext = () =>
@@ -37,18 +34,17 @@ function Slider() {
     return (
         <div>
             <div class="m-2 shadow-xl overflow-hidden rounded-lg">
-                <Image 
-                    fluid={node.childImageSharp.fluid}
+                <GatsbyImage
+                    image={node.childImageSharp.gatsbyImageData}
                     key={node.id}
-                    alt="Slider Images"
-                />
+                    alt="Slider Images" />
             </div>
             <div class="flex w-1/2 justify-between mx-auto">
                 <button class="tracking-wide m-2 inline-block px-3 py-1 rounded-lg shadow-lg bg-red-500 text-white hover:bg-gray-300 hover:text-black" onClick={() => handlePrevious()}>&#11013; Image</button>
                 <button class="tracking-wide m-2 inline-block px-3 py-1 rounded-lg shadow-lg bg-red-500 text-white hover:bg-gray-300 hover:text-black" onClick={() => handleNext()}>Image &#10145;</button>
             </div>
         </div>
-    )
+    );
 };
 
 export default Slider
